6caa3ca98570f77abc1d2ee2058da2944439c2c1
keksobot <keksobot@users.noreply.github.com>
2024-06-05 20:28:36 +0000

    Deploying to gh-pages from @ htmlacademy-adaptive/2470749-cat-energy-31@22a7d244fa2b5cf82b5f0b2aa0ba7cd36bfd2760 🚀

--- 4/scripts/index.js
+++
@@ -1,1 +0,0 @@
-var e=document.querySelector(".main-header__nav"),n=document.querySelector(".main-header__toggle");e.classList.remove("main-header__nav--nojs");n.onclick=function(){e.classList.toggle("main-header__nav--opened")};
